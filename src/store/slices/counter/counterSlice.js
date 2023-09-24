d74dd9fa360ed3f2675f238b1cb4af2392c62c84
Rody Huancas <rodyhuancas.04@gmail.com>
2023-09-23 20:57:54 -0500

    Add: configuration redux

---
+++ src/store/slices/counter/counterSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    value: 0,
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        counter: 10
+    },
+    reducers: {
+        increment: (state) => {
+            state.counter += 1
+        },
+        decrement: (state) => {
+            state.counter -= 1
+        },
+        incrementByTwo: (state, action) => {
+            state.counter += action.payload
+        },
+    },
+})
+
+export const { increment, decrement, incrementByTwo } = counterSlice.actions
+
